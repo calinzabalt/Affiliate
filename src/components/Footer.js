68478530f5b734649997e850ca9bed7e118675e2
Mastercalin <calinzabalt@gmail.com>
2022-11-06 12:37:08 +0200

    blog image and footer logo removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,15 @@ const Footer = () => {
   
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: "14em", height: "10em" }}
-          />
-        </div>
+        { /* 
+          <div className="content has-text-centered">
+            <img
+              src={logo}
+              alt="Kaldi"
+              style={{ width: "14em", height: "10em" }}
+            />
+          </div>
+        */}
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div style={{ maxWidth: "100vw" }} className="columns">
