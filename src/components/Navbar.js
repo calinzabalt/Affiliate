a81c81347e3a9540619c84db1c577d58a3591f68
Mastercalin <calinzabalt@gmail.com>
2022-11-19 13:03:43 +0200

    accesibility

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,6 +21,7 @@ const Navbar = () => {
             className={`navbar-burger burger ${isActive && "is-active"}`}
             aria-expanded={isActive}
             onClick={() => setIsActive(!isActive)}
+            aria-label="Menu"
           >
             <span />
             <span />
